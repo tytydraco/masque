6eb47c9a1952eec452a85cc2c9b5f28ea102c5ad
Tyler Nijmeh <tylernij@gmail.com>
2022-06-13 17:04:01 -0700

    Pretty login screen
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,55 +11,79 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final screenNameController = TextEditingController();
   final roomIdController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   void enterChat() {
-    Navigator
-        .of(context)
-        .push(
-          MaterialPageRoute(
-            builder: (context) => ChatScreen(
-              screenName: screenNameController.text,
-              roomId: roomIdController.text,
+    if (formKey.currentState!.validate()) {
+      Navigator
+          .of(context)
+          .push(
+            MaterialPageRoute(
+              builder: (context) => ChatScreen(
+                screenName: screenNameController.text,
+                roomId: roomIdController.text,
+              )
             )
-          )
-        );
+          );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-            child: TextField(
-              controller: screenNameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Screen name'
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: TextFormField(
+                autofocus: true,
+                textInputAction: TextInputAction.next,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Must have a screen name';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: screenNameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Screen name'
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-            child: TextField(
-              controller: roomIdController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Room id'
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Invalid room id';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: roomIdController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Room id'
+                ),
+                onFieldSubmitted: (_) => enterChat(),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-            child: ElevatedButton(
-              onPressed: enterChat,
-              child: const Text('Enter'),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: ElevatedButton(
+                onPressed: enterChat,
+                child: const Text('Enter'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
