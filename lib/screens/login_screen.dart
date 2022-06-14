90ac5c21e42d94bd8986facedba9499c5ae64f55
Tyler Nijmeh <tylernij@gmail.com>
2022-06-13 17:18:26 -0700

    Limit max width
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -31,58 +31,63 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-              child: TextFormField(
-                autofocus: true,
-                textInputAction: TextInputAction.next,
-                validator: (text) {
-                  if (text == null || text.isEmpty) {
-                    return 'Must have a screen name';
-                  } else {
-                    return null;
-                  }
-                },
-                controller: screenNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Screen name'
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 300),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+                  child: TextFormField(
+                    autofocus: true,
+                    textInputAction: TextInputAction.next,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Must have a screen name';
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: screenNameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Screen name'
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-              child: TextFormField(
-                textInputAction: TextInputAction.done,
-                validator: (text) {
-                  if (text == null || text.isEmpty) {
-                    return 'Invalid room id';
-                  } else {
-                    return null;
-                  }
-                },
-                controller: roomIdController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Room id'
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+                  child: TextFormField(
+                    textInputAction: TextInputAction.done,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Invalid room id';
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: roomIdController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Room id'
+                    ),
+                    onFieldSubmitted: (_) => enterChat(),
+                  ),
                 ),
-                onFieldSubmitted: (_) => enterChat(),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+                  child: ElevatedButton(
+                    onPressed: enterChat,
+                    child: const Text('Enter'),
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-              child: ElevatedButton(
-                onPressed: enterChat,
-                child: const Text('Enter'),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
