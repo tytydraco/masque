a50e46cb21f37524cc141ae99b11f32b1b991852
Tyler Nijmeh <tylernij@gmail.com>
2022-06-14 13:55:53 -0700

    Separate widget for login form
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:masque/screens/chat_screen.dart';
+import 'package:masque/widgets/login_form_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,85 +10,25 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final screenNameController = TextEditingController();
-  final roomIdController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-
-  void enterChat() {
-    if (formKey.currentState!.validate()) {
-      Navigator
-          .of(context)
-          .push(
-            MaterialPageRoute(
-              builder: (context) => ChatScreen(
-                screenName: screenNameController.text,
-                roomId: roomIdController.text,
-              )
+  void enterChat(String screenName, String roomId) {
+    Navigator
+        .of(context)
+        .push(
+          MaterialPageRoute(
+            builder: (context) => ChatScreen(
+              screenName: screenName,
+              roomId: roomId,
             )
-          );
-    }
+          )
+        );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 300),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-                  child: TextFormField(
-                    autofocus: true,
-                    textInputAction: TextInputAction.next,
-                    validator: (text) {
-                      if (text == null || text.isEmpty) {
-                        return 'Must have a screen name';
-                      } else {
-                        return null;
-                      }
-                    },
-                    controller: screenNameController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Screen name'
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-                  child: TextFormField(
-                    textInputAction: TextInputAction.done,
-                    validator: (text) {
-                      if (text == null || text.isEmpty) {
-                        return 'Invalid room id';
-                      } else {
-                        return null;
-                      }
-                    },
-                    controller: roomIdController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Room id'
-                    ),
-                    onFieldSubmitted: (_) => enterChat(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-                  child: ElevatedButton(
-                    onPressed: enterChat,
-                    child: const Text('Enter'),
-                  ),
-                )
-              ],
-            ),
-          ),
+        child: LoginFormWidget(
+          onLogin: enterChat,
         ),
       ),
     );
---
+++ lib/widgets/login_form_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginFormWidget extends StatefulWidget {
+  final Function(String, String) onLogin;
+
+  const LoginFormWidget({
+    Key? key,
+    required this.onLogin,
+  }) : super(key: key);
+
+  @override
+  State<LoginFormWidget> createState() => _LoginFormWidgetState();
+}
+
+class _LoginFormWidgetState extends State<LoginFormWidget> {
+  final screenNameController = TextEditingController();
+  final roomIdController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void _onLogin() {
+    if (formKey.currentState!.validate()) {
+      widget.onLogin(
+        screenNameController.text,
+        roomIdController.text,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: TextFormField(
+                autofocus: true,
+                textInputAction: TextInputAction.next,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Must have a screen name';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: screenNameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Screen name'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Invalid room id';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: roomIdController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Room id'
+                ),
+                onFieldSubmitted: (_) => _onLogin(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: ElevatedButton(
+                onPressed: _onLogin,
+                child: const Text('Enter'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
