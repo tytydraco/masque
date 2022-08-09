f00abd794a68c9e72da434fb10f6d6d16eb0ec18
Tyler Nijmeh <tylernij@gmail.com>
2022-08-09 13:38:32 -0700

    Add method to set default shared object values
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/data/shared_objects.dart
+++ lib/src/data/shared_objects.dart
@@ -2,11 +2,11 @@ import 'package:shared_objects/shared_objects.dart';
 
 /// A class to hold instances of all the global shared objects.
 class SharedObjects {
-  /// Whether or not to save these login credentials.
-  final saveLogin = SharedBool('save_login');
-
   /// The saved screen name.
   final screenName = SharedString('screen_name');
+
+  /// Whether or not to save these login credentials.
+  final saveLogin = SharedBool('save_login');
 
   /// The saved room id.
   final roomId = SharedString('room_id');
@@ -16,4 +16,14 @@ class SharedObjects {
 
   /// Whether or not to obscure the room id.
   final obscureRoomId = SharedBool('obscure_room_id');
+
+  /// Set all shared object values to a default value if a value is not yet set.
+  Future<void> setDefaultsIfNull() async {
+    await screenName.mutate((initial) => initial ?? '');
+    await roomId.mutate((initial) => initial ?? '');
+
+    await saveLogin.mutate((initial) => initial ?? true);
+    await multiline.mutate((initial) => initial ?? false);
+    await obscureRoomId.mutate((initial) => initial ?? true);
+  }
 }
